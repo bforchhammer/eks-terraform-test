09d90f18f98f6cb6ba5b4c88e9dbb2b71f0a855c
Benedikt Forchhammer <b.forchhammer@gmail.com>
2019-06-17 10:17:01 +0200

    Applied terraform fmt

--- eks-worker-nodes.tf
+++ eks-worker-nodes.tf
@@ -28,17 +28,17 @@ POLICY
 
 resource "aws_iam_role_policy_attachment" "demo-node-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = "${aws_iam_role.demo-node.name}"
+  role = "${aws_iam_role.demo-node.name}"
 }
 
 resource "aws_iam_role_policy_attachment" "demo-node-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = "${aws_iam_role.demo-node.name}"
+  role = "${aws_iam_role.demo-node.name}"
 }
 
 resource "aws_iam_role_policy_attachment" "demo-node-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = "${aws_iam_role.demo-node.name}"
+  role = "${aws_iam_role.demo-node.name}"
 }
 
 resource "aws_iam_instance_profile" "demo-node" {
@@ -47,53 +47,53 @@ resource "aws_iam_instance_profile" "demo-node" {
 }
 
 resource "aws_security_group" "demo-node" {
-  name        = "terraform-eks-demo-node"
+  name = "terraform-eks-demo-node"
   description = "Security group for all nodes in the cluster"
-  vpc_id      = "${aws_vpc.demo.id}"
+  vpc_id = "${aws_vpc.demo.id}"
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = "${
     map(
-     "Name", "terraform-eks-demo-node",
-     "kubernetes.io/cluster/${var.cluster-name}", "owned",
+      "Name", "terraform-eks-demo-node",
+      "kubernetes.io/cluster/${var.cluster-name}", "owned",
     )
   }"
 }
 
 resource "aws_security_group_rule" "demo-node-ingress-self" {
-  description              = "Allow node to communicate with each other"
-  from_port                = 0
-  protocol                 = "-1"
-  security_group_id        = "${aws_security_group.demo-node.id}"
+  description = "Allow node to communicate with each other"
+  from_port = 0
+  protocol = "-1"
+  security_group_id = "${aws_security_group.demo-node.id}"
   source_security_group_id = "${aws_security_group.demo-node.id}"
-  to_port                  = 65535
-  type                     = "ingress"
+  to_port = 65535
+  type = "ingress"
 }
 
 resource "aws_security_group_rule" "demo-node-ingress-cluster" {
-  description              = "Allow worker Kubelets and pods to receive communication from the cluster control plane"
-  from_port                = 1025
-  protocol                 = "tcp"
-  security_group_id        = "${aws_security_group.demo-node.id}"
+  description = "Allow worker Kubelets and pods to receive communication from the cluster control plane"
+  from_port = 1025
+  protocol = "tcp"
+  security_group_id = "${aws_security_group.demo-node.id}"
   source_security_group_id = "${aws_security_group.demo-cluster.id}"
-  to_port                  = 65535
-  type                     = "ingress"
+  to_port = 65535
+  type = "ingress"
 }
 
 data "aws_ami" "eks-worker" {
   filter {
-    name   = "name"
+    name = "name"
     values = ["amazon-eks-node-${aws_eks_cluster.demo.version}-v*"]
   }
 
   most_recent = true
-  owners      = ["602401143452"] # Amazon EKS AMI Account ID
+  owners = ["602401143452"] # Amazon EKS AMI Account ID
 }
 
 # EKS currently documents this required userdata for EKS worker nodes to
